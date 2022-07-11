acf63064ebf97f02c938d447bb4d548911b321d6
楠ちゃん <amenai@bwmc.live>
2022-07-11 08:29:51 +0800

    Remove Useless Code (Ver1.4)

--- js/set.js
+++ js/set.js
@@ -40,29 +40,7 @@ function setBgImgInit() {
 
     switch (bg_img["type"]) {
         case "1":
-            /**var pictures = new Array();
-            *pictures[0] = './img/background1.webp';
-            *pictures[1] = './img/background2.webp';
-            *pictures[2] = './img/background3.webp';
-            *pictures[3] = './img/background4.webp';
-            *pictures[4] = './img/background5.webp';
-            *pictures[5] = './img/background6.webp';
-            *pictures[6] = './img/background7.webp';
-            *pictures[7] = './img/background8.webp';
-            *pictures[8] = './img/background9.webp';
-            *pictures[9] = './img/background10.webp';
-            *var rd = Math.floor(Math.random() * 10);
-            */
             $('#bg').attr('src', 'https://api.bwmc.live/pc/') //随机默认壁纸
-            break;
-        case "2":
-            $('#bg').attr('src', 'https://api.dujin.org/bing/1920.php'); //必应每日
-            break;
-        case "3":
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/gqapi/gqapi.php'); //随机风景
-            break;
-        case "4":
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/api/api.php'); //随机动漫
             break;
     }
 }
@@ -78,49 +56,7 @@ $(document).ready(function () {
 
         if (type === "1") {
             setBgImg(bg_img);
-            var pictures = new Array();
-            pictures[0] = './img/background1.webp';
-            pictures[1] = './img/background2.webp';
-            pictures[2] = './img/background3.webp';
-            pictures[3] = './img/background4.webp';
-            pictures[4] = './img/background5.webp';
-            pictures[5] = './img/background6.webp';
-            pictures[6] = './img/background7.webp';
-            pictures[7] = './img/background8.webp';
-            pictures[8] = './img/background9.webp';
-            pictures[9] = './img/background10.webp';
-            var rd = Math.floor(Math.random() * 10);
-            $('#bg').attr('src', pictures[rd]) //随机默认壁纸
-            iziToast.show({
-                icon: "fa-solid fa-image",
-                timeout: 2500,
-                message: '壁纸设置成功',
-            });
-        }
-
-        if (type === "2") {
-            setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.dujin.org/bing/1920.php'); //必应每日
-            iziToast.show({
-                icon: "fa-solid fa-image",
-                timeout: 2500,
-                message: '壁纸设置成功',
-            });
-        }
-
-        if (type === "3") {
-            setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/gqapi/gqapi.php'); //随机风景
-            iziToast.show({
-                icon: "fa-solid fa-image",
-                timeout: 2500,
-                message: '壁纸设置成功',
-            });
-        }
-
-        if (type === "4") {
-            setBgImg(bg_img);
-            $('#bg').attr('src', 'https://api.ixiaowai.cn/api/api.php'); //随机动漫
+            $('#bg').attr('src', 'https://api.bwmc.live/pc/') //随机默认壁纸
             iziToast.show({
                 icon: "fa-solid fa-image",
                 timeout: 2500,
