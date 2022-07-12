db545434547ec1af1f55d2cadbcc6d30ae5ee61b
楠ちゃん <amenai@bwmc.live>
2022-07-12 11:07:39 +0800

    Update to Ver1.5.1.4
    I don't want to touch the <div> any more.

--- js/music.js
+++ js/music.js
@@ -11,7 +11,7 @@ GitHub：https://github.com/imsyy/home
 */
 var server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 var type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-var id = "975427390"; //封面 ID / 单曲 ID / 歌单 ID
+var id = "7533625421"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id,
@@ -23,7 +23,7 @@ $.ajax({
             order: 'random',
             preload: 'auto',
             listMaxHeight: '336px',
-            volume: '0.5',
+            volume: '0.4',
             mutex: true,
             lrcType: 3,
             audio: data,
