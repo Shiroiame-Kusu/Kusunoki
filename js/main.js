cbd96010d79d051fa27dc0b1e0ab0448cadaeed9
楠ちゃん <amenai@bwmc.live>
2022-08-19 16:08:16 +0800

    Ver 1.6.0-R5: Daily Update

--- js/main.js
+++ js/main.js
@@ -365,7 +365,7 @@ color: rgb(0,255,255);
 var title1 = 'BWMC OOM PART'
 var title2 = 'Edited By Shiroiame Kusu'
 var content = `
-版 本 号：1.6.0-4
+版 本 号：1.6.0-R5
 更新日期：Aug.9th.2022
 更新内容：（小版本都是小修小补）
 1.6: 同步上游项目更新
