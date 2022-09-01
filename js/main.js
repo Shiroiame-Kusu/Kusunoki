9f1ab5abd633458f72565fdb8f886fe9d1e48583
楠ちゃん <amenai@bwmc.live>
2022-09-01 17:36:02 +0800

    Ver 2.1.0-B2: Change the 404 Page

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ GitHub：https://github.com/Shiroiame-Kusu/Kusunoki
 */
 //声明版本
 var release = '2.1.0'
-var version = 'B1'
+var version = 'B2'
 var final_date = 'Sept.1st.2022'
 var final_version = release + '-' + version
 var final_version_img = '当前 v' + release + '_' + version
